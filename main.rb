300c44f16bfa0aeb28f85fb72e9e9ba5b34f6497
unikounio <bubbbble428@gmail.com>
2024-03-04 15:57:14 +0900

    進行方法の変更に合わせてmainファイルを修正した

--- main.rb
+++ main.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-require 'csv'
-
 Dir.glob(File.join('./lib', '*.rb')).each do |file|
   require file
-end
-
-Dir.glob(File.join(File.expand_path('./data', __dir__), '*.csv')).each do |file|
-  puts file
-
-  CSV.foreach(file) do |row|
-    puts row.inspect
-  end
 end
